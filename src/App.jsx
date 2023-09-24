262bcbd202ca13c17b547193c7a2357721d721f2
Emill <lemito66@gmail.com>
2023-09-24 16:10:24 -0500

    enrutamiento de la aplicación

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
-import { useState } from 'react'
-
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import ProductsPage from "./pages/ProductsPage";
+import ProductsFormPage from "./pages/ProductsFormPage";
+import Navigation from "./components/Navigation";
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <h1>Hello, world!</h1>
+      <BrowserRouter>
+        <Navigation />
+        <Routes>
+          <Route path="/" element={<Navigate to="/products" />} />
+          <Route path="/products" element={<ProductsPage />} />
+          <Route path="/products-create" element={<ProductsFormPage />} />
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
