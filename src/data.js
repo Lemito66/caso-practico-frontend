7e096c8b9a1d4a39af2444ca8081d531feeb013f
Emill <lemito66@gmail.com>
2023-09-24 16:09:57 -0500

    titulos de tabla

---
+++ src/data.js
@@ -0,0 +1,8 @@
+export const TABLEHEADERS = [
+    { id: 1, name: 'Logo' },
+    { id: 2, name: 'Nombre del producto' },
+    { id: 3, name: 'Descripción' },
+    { id: 4, name: 'Fecha de liberación' },
+    { id: 5, name: 'Fecha de reestructuración' },
+    { id: 6, name: '' },
+]
